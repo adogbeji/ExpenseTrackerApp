e068e96fcd38d3b0b1c5ac86c7afc2fa263c8f11
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-03-10 11:06:53 +0000

    Add starter code & set SDK version to 2.11.1

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+
+              controller: titleController,
+
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+
+              controller: amountController,
+
+              // onChanged: (val) => amountInput = val,  // Anonymous function shorthand
+            ),
+            TextButton(
+              child: Text('Add Transaction'),
+              style: TextButton.styleFrom(
+                primary: Colors.purple,
+              ),
+
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+
+              // onPressed: () {
+              //   print(titleInput);
+              //   print(amountInput);
+              // },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
