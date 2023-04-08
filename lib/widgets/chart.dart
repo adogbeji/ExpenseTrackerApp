b02463d38e5f75e5c19e5f2d77519cc24898d5fa
adogbeji <baka_1993@yahoo.co.uk>
2023-04-08 16:16:28 +0100

    Test getter by printing it

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -30,6 +30,7 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionValues);  // Testing
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
